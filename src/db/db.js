fb74de698fd013e77edae1622cddec651cad44f9
galdinoM <54222781+galdinoM@users.noreply.github.com>
2023-10-08 13:58:58 -0300

    primeiro commit

--- src/db/db.js
+++ src/db/db.js
@@ -2,9 +2,6 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
 
